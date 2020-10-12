b1afc666c6d230bdb92b23625d711f65ca953e9b
Miruna Cotet <miruna.cotet@gmail.com>
2020-10-11 20:47:08 -0400

    day 2 test 1

--- public/js/coordination_game.js
+++ public/js/coordination_game.js
@@ -21,6 +21,18 @@ eu = [
 ];
 
 
+downloadCSV = function (csv, filename) {
+    var csvFile;
+    var downloadLink;
+
+    csvFile = new Blob([csv], { type: "text/csv" });
+    downloadLink = document.createElement("a");
+    downloadLink.download = filename;
+    downloadLink.href = window.URL.createObjectURL(csvFile);
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+};
 
 /**************/
 /** Constants */
@@ -942,18 +954,7 @@ var riskSurvey = {
     }
 };
 
-downloadCSV = function(csv,filename) {
-    var csvFile;
-    var downloadLink;
-    
-    csvFile = new Blob( [csv], {type: "text/csv"});
-    downloadLink = document.createElement("a");
-    downloadLink.download = filename;
-    downloadLink.href = window.URL.createObjectURL(csvFile);
-    downloadLink.style.display = "none";
-    document.body.appendChild(downloadLink);
-    downloadLink.click();
-    };
+
 
 
 
